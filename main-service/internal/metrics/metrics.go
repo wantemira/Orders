5bb50148d6f9a4677fa02cddedb5a3a6abad2b6f
achi3v3 <amir-tutaev@mail.ru>
2026-01-20 15:25:17 +0500

    feat(back): create metrics handlers

---
+++ main-service/internal/metrics/metrics.go
@@ -0,0 +1,50 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	OrdersCreatedTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "orders_created_total",
+			Help: "Total number of created orders",
+		},
+		[]string{"status"}, // success, error
+	)
+
+	OrderProcessingDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "order_processing_duration_seconds",
+			Help:    "Time spent processing order",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"handler"},
+	)
+
+	OrdersInCache = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "orders_in_cache",
+			Help: "Number of orders currently in cache",
+		},
+	)
+
+	KafkaProcessingAttempts = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "kafka_processing_attempts",
+			Help:    "Number of attempts needed to process Kafka message",
+			Buckets: prometheus.LinearBuckets(1, 1, 10),
+		},
+		[]string{"topic", "final_status"},
+	)
+
+	KafkaMessageProcessingDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "kafka_message_processing_duration_seconds",
+			Help:    "Time spent processing Kafka message from receive to commit/final error",
+			Buckets: prometheus.ExponentialBuckets(0.1, 2, 10),
+		},
+		[]string{"topic", "status"},
+	)
+)
