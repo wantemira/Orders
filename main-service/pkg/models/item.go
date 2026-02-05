5e34ec93159eb60e25e0e53abfc748db82ed4d21
achi3v3 <amir-tutaev@mail.ru>
2026-02-05 23:06:16 +0500

    fix(back): add max values for int64 fields

--- main-service/pkg/models/item.go
+++ main-service/pkg/models/item.go
@@ -4,7 +4,7 @@ package models
 // Item представляет товар в заказе
 type Item struct {
 	ID          uint   `json:"id" validate:"-"` // - означает "не валидировать"
-	ChrtID      int64  `json:"chrt_id" validate:"required,min=1"`
+	ChrtID      int64  `json:"chrt_id" validate:"required,min=1,max=2147483647"`
 	TrackNumber string `json:"track_number" validate:"required,alphanumunicode,max=50"`
 	Price       int    `json:"price" validate:"required,min=0,max=1000000"`
 	RID         string `json:"rid" validate:"required,max=50"`
@@ -12,7 +12,7 @@ type Item struct {
 	Sale        int    `json:"sale" validate:"min=0,max=100"`
 	Size        string `json:"size" validate:"max=10"`
 	TotalPrice  int    `json:"total_price" validate:"required,min=0,max=1000000"`
-	NmID        int64  `json:"nm_id" validate:"required,min=1"`
+	NmID        int64  `json:"nm_id" validate:"required,min=1,max=2147483647"`
 	Brand       string `json:"brand" validate:"max=100"`
 	Status      int    `json:"status" validate:"min=0,max=999"`
 }
